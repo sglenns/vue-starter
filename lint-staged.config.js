c61a7ba7afa944bcd13ceecc194f62bd3b11b845
Glenn Mendoza <g.mendoza@nephila.digital>
2025-11-18 12:15:22 +0100

    initial commit

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+export default {
+  "{src,tests}/**/*.{css,scss,md}": ["prettier --write"],
+  "{src,tests}/**/*.{json,js,jsx}": ["prettier --write", "eslint --fix"],
+  "{src,tests}/**/*.{ts,tsx,vue}": [
+    "prettier --write",
+    "eslint --fix",
+    () => `vue-tsc --build --force`,
+  ],
+};
