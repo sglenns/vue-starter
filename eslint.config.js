73984e67c3b2196e9ea31f0040569364353a6aed
Glenn Mendoza <g.mendoza@nephila.digital>
2025-11-28 11:55:54 +0100

    style: update dependencies and eslint settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
+import css from "@eslint/css";
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import baselineJs, { BASELINE } from "eslint-plugin-baseline-js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
@@ -8,17 +10,48 @@ import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
   },
-  tseslint.configs.recommended,
-  pluginVue.configs["flat/essential"],
+
+  {
+    files: ["**/*.css"],
+    language: "css/css",
+    plugins: { css },
+    extends: ["css/recommended"],
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js, "baseline-js": baselineJs },
+    extends: [
+      "js/recommended",
+      tseslint.configs.recommended,
+      baselineJs.configs.recommended({ available: BASELINE.WIDELY }),
+    ],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+
   {
     files: ["**/*.vue"],
     languageOptions: { parserOptions: { parser: tseslint.parser } },
+    extends: [pluginVue.configs["flat/recommended"]],
   },
+
   eslintConfigPrettier,
   eslintPluginPrettierRecommended,
 ]);
